83943ee9069660c3df0dbd273f6951c9b0ba7f07
Szilmaril <48657741+Szilmaril@users.noreply.github.com>
2019-03-28 19:28:32 +0100

    Update uploadBook.php

--- konyvtar/upload/uploadBook.php
+++ konyvtar/upload/uploadBook.php
@@ -210,32 +210,32 @@
         if(isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
             if($check !== false) {
-                echo "File is an image - " . $check["mime"] . ".";
+                
                 $uploadOk = 1;
             } else {
-                echo "File is not an image.";
+                echo "<script>window.location.href='uploadBook.php?error=nopict'</script>";
                 $uploadOk = 0;
             }
         }
 // Check if file already exists
         if (file_exists($target_file)) {
-            echo "Sorry, file already exists.";
+            echo "<script>window.location.href='uploadBook.php?error=already'</script>";
             $uploadOk = 0;
         }
 // Check file size
         if ($_FILES["fileToUpload"]["size"] > 5000000) {
-            echo "Sorry, your file is too large.";
+            echo "<script>window.location.href='uploadBook.php?error=big'</script>";
             $uploadOk = 0;
         }
 // Allow certain file formats
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
             && $imageFileType != "gif" ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            echo "<script>window.location.href='uploadBook.php?error=variation'</script>";
         $uploadOk = 0;
     }
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "<script>window.location.href='uploadBook.php?error=wrong'</script>";
 // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -263,8 +263,9 @@
 			'".$category_id."')";
             $query = $db->query($uploadPictureSql);
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "<script>window.location.href='uploadBook.php?error=fault'</script>";
         }
+		echo "<script>window.location.href='uploadBook.php?success=done'</script>";
     }
 
 }
