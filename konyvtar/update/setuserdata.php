a6a424e8e6dbf9060435e9e26278e3ccc184038f
Pethimre <petho.9884@gmail.com>
2019-03-31 17:33:54 +0200

    Update setuserdata.php

--- konyvtar/update/setuserdata.php
+++ konyvtar/update/setuserdata.php
@@ -56,15 +56,6 @@ else
 				echo "<script>window.location.href='listMyUser.php?error=noMatch'</script>";
 			}
 		}
-		elseif (empty($newPassword2) && empty($newPassword)) {
-			$updateWithNewPasswordQuery = "UPDATE `users` SET `email` = '$email', `birthday` = '$birthday' WHERE `users`.`id` = ".$userid;
-				$update = $db->query($updateWithNewPasswordQuery);
-				echo "<script>window.location.href='listMyUser.php?success=done'</script>";
-			}
-			else
-			{
-				echo "<script>window.location.href='listMyUser.php?error=invalidPW'</script>";
-			}
 		}
 		else
 		{
